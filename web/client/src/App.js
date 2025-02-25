d3d8788e4f387d51dd0e460744ffc08074b81852
Voronin.M. <voronin.m@energosoft.net>
2025-02-25 13:11:48 +0300

    test app build v1

---
+++ web/client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './van.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the first version of the test react application Deal-do
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
